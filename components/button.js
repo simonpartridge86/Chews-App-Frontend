9110281c6123a267d7fa51d1b0f66a0d744a9916
mauriziomonti <mauriziomonti@hotmail.com>
2022-07-28 11:57:31 +0100

    created first button mockup

--- components/button.js
+++ components/button.js
@@ -1,1 +1,28 @@
-// helloooo
+import { Button, ButtonProps, Flex, useColorModeValue } from '@chakra-ui/react';
+
+
+export default function ClickMe({ButtonProps}) {
+
+  return (
+    
+      <Button
+        {...ButtonProps}
+        px={8}
+        bg={'#FD2B46'}
+        color={'white'}
+        rounded={'md'}
+        _hover={{
+          transform: 'translateY(-2px)',
+          boxShadow: 'lg',
+        }}
+        >
+        Click Me
+      </Button>
+  );
+}
+
+/*brand: {
+    'primary-color': '#FD2B46',
+    'light-color': '#FFFFFF',
+    'dark-color': '#32373B',
+  }*/
