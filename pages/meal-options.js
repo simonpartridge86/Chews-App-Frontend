a1994c6dfdd517e5dbc75e5c138f569d281e5ef4
simonpartridge86 <simonpartridge86@gmail.com>
2022-07-29 14:31:30 +0100

    Added positioning on options page. > > Co-authored-by: Maurizio Monti <mauriziomonti@hotmail.com>

--- pages/meal-options.js
+++ pages/meal-options.js
@@ -5,23 +5,49 @@ import React from "react";
 
 export default function MealOptions() {
   return (
-    <div>
-      <h1 className="font-permanent-marker">Chews an option:</h1>
-      <VStack
-        spacing={4}
-        align="center"
-      >
-        <MainButton buttonText="Breakfast" buttonSize="md" colorMode="dark" buttonWidth="80%"/>
-        <MainButton buttonText="Main" buttonSize="md" colorMode="dark" buttonWidth="80%"/>
-        <MainButton buttonText="Dessert" buttonSize="md" colorMode="dark" buttonWidth="80%"/>
-        <MainButton buttonText="Just Chews" buttonSize="md" colorMode="dark" buttonWidth="80%"/>
-        <MainButton
-          buttonText="Search by Ingredients"
-          buttonSize="md"
-          colorMode="dark"
-          buttonWidth="80%"
-        />
-      </VStack>
+    <div className="h-screen">
+      <nav className="h-1/6 text-center align-center font-permanent-marker">
+        Nav Bar Placeholder
+      </nav>
+      <main className="flex flex-col h-5/6 justify-around">
+        <VStack spacing={4} align="center">
+          <h1 className="font-permanent-marker text-center ">
+            Chews <span className="font-nunito font-bold">an option:</span>
+          </h1>
+          <MainButton
+            buttonText="Breakfast"
+            buttonSize="lg"
+            colorMode="dark"
+            buttonWidth="80%"
+          />
+          <MainButton
+            buttonText="Main"
+            buttonSize="lg"
+            colorMode="dark"
+            buttonWidth="80%"
+          />
+          <MainButton
+            buttonText="Dessert"
+            buttonSize="lg"
+            colorMode="dark"
+            buttonWidth="80%"
+          />
+        </VStack>
+        <VStack spacing={4} align="center">
+          <MainButton
+            buttonText="Just Chews"
+            buttonSize="lg"
+            colorMode="dark"
+            buttonWidth="80%"
+          />
+          <MainButton
+            buttonText="Search by Ingredients"
+            buttonSize="lg"
+            colorMode="dark"
+            buttonWidth="80%"
+          />
+        </VStack>
+      </main>
     </div>
   );
 }
