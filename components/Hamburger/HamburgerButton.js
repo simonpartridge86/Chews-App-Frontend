fee512d49d447cd44a41829530a6065468d74f96
Adam P <adam.phasey@hotmail.com>
2022-07-29 14:18:59 +0100

    removed unused code from previous attempted menu

--- components/Hamburger/HamburgerButton.js
+++ components/Hamburger/HamburgerButton.js
@@ -11,30 +11,7 @@ import {
   Avatar,
   Switch,
 } from "@chakra-ui/react";
-{
-  /* <Menu pos={'fixed'} top="1rem" autoSelect={false}>
-  <MenuButton
-    as={IconButton}
-    aria-label='Options'
-    icon={<HamburgerIcon />}
-    variant='outline'
-  />
-  <MenuList bgColor={'#FD2B46'} color={"#fff"} w="100vw" h='100vh'>
-    <MenuItem minH='25vh' icon={<AddIcon />} command='⌘T'>
-      Home
-    </MenuItem>
-    <MenuItem minH='25vh' icon={<ExternalLinkIcon />} command='⌘N'>
-      Find Chews
-    </MenuItem>
-    <MenuItem minH='25vh' icon={<RepeatIcon />} command='⌘⇧N'>
-      My Favourites
-    </MenuItem>
-    <MenuItem minH='25vh' icon={<EditIcon />} command='⌘O'>
-      My Profile
-    </MenuItem>
-  </MenuList>
-</Menu> */
-}
+
 
 import { useState } from "react";
 import {
@@ -100,7 +77,7 @@ export default function Hamburger() {
         w="100vw"
         display={display}
         zIndex={20}
-        h="100vh"
+        h="75vh"
         pos="fixed"
         top="0"
         left="0"
