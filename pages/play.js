85a396addd70f13deb636a1b7d115fe86d242fbc
Adam P <adam.phasey@hotmail.com>
2022-08-01 16:16:25 +0100

    Finished trello tasks 404 libs and recipe card has begun construction

--- pages/play.js
+++
@@ -1,6 +0,0 @@
-import Link from 'next/link'
-import SocialProfileWithImageHorizontal from '../components/RecipeCard/RecipeCard'
-
-export default function Play() {
-  return <SocialProfileWithImageHorizontal />        
-}
