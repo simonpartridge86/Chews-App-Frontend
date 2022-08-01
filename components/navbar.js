26693c0647a6f320e8e251e9a1e2ffbb3b402ba6
samsonhumber <printerfissh693@gmail.com>
2022-08-01 11:04:13 +0100

    Flex-row on navbar js, need Adam's help now
    Co-authored-by: Mino <mino.devito@gmail.com>
    Co-authored-by: Sam <printerfissh693@gmail.com>

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,13 @@
 //import { Heading } from '@chakra-ui/react'
 import ChewsLogo from './chews-logo';
 import Hamburger from './Hamburger/HamburgerButton';
+import "./pic.jpg";
 
 export default function Navbar() {
   return (
-    <div>
+    <div className='flex flex-row'>
     <ChewsLogo theme={"brand.primary"} size={"4xl"}/>
-    <picture><img src= "pic.jpg" alt="girl with sweets"></img></picture> 
+    <picture><img src= "./pic.jpg" alt="girl with sweets"></img></picture> 
     <Hamburger/>
     </div>
   );
