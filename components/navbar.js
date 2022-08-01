f6bb214a806bf1557ce0a6cfdd143a8d8102eb34
Mino Devito <mino.devito@gmail.com>
2022-08-01 12:15:44 +0100

    more flex need review on image

--- components/navbar.js
+++ components/navbar.js
@@ -1,14 +1,14 @@
 //import { Heading } from '@chakra-ui/react'
 import ChewsLogo from './chews-logo';
 import Hamburger from './Hamburger/HamburgerButton';
-import "./pic.jpg";
+import Testpic from '../public/pic.jpg';
 
 export default function Navbar() {
   return (
-    <div className='flex flex-row'>
-    <ChewsLogo theme={"brand.primary"} size={"4xl"}/>
-    <picture><img src= "./pic.jpg" alt="girl with sweets"></img></picture> 
+    <div className='flex flex-row justify-around'>
     <Hamburger/>
+    <ChewsLogo theme={"brand.primary"} size={"3xl"}/>
+    <img src={Testpic} alt="girl with sweets" className='w-6'></img>
     </div>
   );
 }
