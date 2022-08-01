d7d74afad0de883601420a2917760c1c46b79ed5
mauriziomonti <mauriziomonti@hotmail.com>
2022-08-01 13:54:01 +0100

    completed a 404 page, added libs data Co-authored-by: Adam Phasey  <adam.phasey@hotmail.com>

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+// pages/404.js
+import Link from 'next/link'
+
+export default function Custom404() {
+  return (
+    <>
+        <h1>404 - Page Not Found</h1>
+        <h2>Uh-Oh.. looks like your Chews-ing ain't correct.</h2>
+        <ChewsLogo theme={"brand.primary"} size={"3xl"} />
+        <p>Home <Link href="/" ><a>Home</a></Link> </p>
+    </>
+)}
