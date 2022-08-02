f1849ad4e744103a646e149388b2070f738e6a96
mauriziomonti <mauriziomonti@hotmail.com>
2022-08-02 10:11:55 +0100

    adding a play file to try things out

---
+++ pages/play.js
@@ -0,0 +1,11 @@
+// pages/404.js
+import Link from 'next/link'
+import SocialProfileWithImageHorizontal from '../components/RecipeCard/RecipeCard'
+
+
+export default function Play() {
+  return (
+    <>
+        <SocialProfileWithImageHorizontal />
+    </>
+)}
