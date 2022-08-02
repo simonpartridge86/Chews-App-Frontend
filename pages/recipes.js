018e56d58904a5a6c481afc467e3ae3863d68e3e
samsonhumber <printerfissh693@gmail.com>
2022-08-02 11:44:26 +0100

    Put in place You are making h2, title h3, meal image tags
    Co-authored-by: Mino <mino.devito@gmail.com>
    Co-authored-by: Sam <printerfissh693@gmail.com>

--- pages/recipes.js
+++ pages/recipes.js
@@ -1,26 +1,33 @@
-import MainButton from "../components/MainButton";
 import { useState } from "react";
 import { recipeData } from "../libs/recipes/recipes";
 import { Link } from '@chakra-ui/react'
 import { ArrowBackIcon } from '@chakra-ui/icons'
+import { useRouter } from "next/router";
 
 import React from "react";
 
-export default function Recipes() {
-  return (
-    <div className="flex flex-col">
-      <Link>Back to results<ArrowBackIcon></ArrowBackIcon></Link>
-    </div>
-  );
+function Recipes() {
+  return(<div className="flex flex-col items-center">
+  <Link>Back to results <ArrowBackIcon/></Link>
+  <h2 className="color-primary font-permanent-marker text-2xl">You are making:</h2>
+  <h3 className="color-dark font-nunito text-lg">{recipeData[0].recipe.label}</h3>
+  <img
+   src='https://www.alrightnow.com/wp-content/uploads/2018/03/Hearty-Chickpea-Salad-Pic-scaled.jpeg'
+   alt={recipeData[0].recipe.label}
+   className="w-4/5">
+   </img>
+</div>)
 }
 
+export default Recipes;
 
 /* PLAN:
 - Find dummy data ✅
-  - Toolbar (from yesterday) - burger, chews logo, profile pic
-  - 'Back to results' link with <-- arrow
-  - 'You are making' header, perm-marker, primary
-  - 'Creamy steak alfredo' nunito, dark
+- Initialise elements of the page (one tick) and make fully functional (two ticks)
+  - Toolbar (from yesterday) - burger, chews logo, profile pic ✅
+  - 'Back to results' link with <-- arrow ✅
+  - 'You are making' header, perm-marker, primary ✅ALMOST
+  - 'Creamy steak alfredo' nunito, dark ✅
   - Picture of meal, 80% screen-width
   - Add to favourites button
   - 'Things you need' primary, perm-marker, header
