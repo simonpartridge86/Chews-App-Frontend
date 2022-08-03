ee9922c7db641d53d3a8ca85165fe449f41bbd4c
simonpartridge86 <simonpartridge86@gmail.com>
2022-08-03 11:55:26 +0100

    meal select page styling updated.
    Co-authored-by: Kunal <kun.shukla@gmail.com>

--- pages/meal-select.js
+++ pages/meal-select.js
@@ -8,50 +8,56 @@ export default function MealSelect() {
   const router = useRouter();
 
   return (
-    <main className="flex flex-col h-screen justify-center">
+    <main className="flex flex-col h-[80vh] justify-center items-center">
       <VStack spacing={4} align="center">
-        <h1 className="font-permanent-marker text-center text-2xl">
-          Chews <span className="font-nunito font-bold">an option:</span>
+        <h1 className="font-nunito font-bold text-2xl">
+          First,{" "}
+          <span className="font-permanent-marker text-center text-2xl">
+            Chews{" "}
+          </span>
+          a meal type:
         </h1>
-        <MainButton
-          buttonText="Breakfast"
-          buttonSize="lg"
-          colorMode="dark"
-          buttonWidth="80%"
-          onClick={() => {
-            const meal = "breakfast";
-            router.push({
-              pathname: "/search-select",
-              query: { meal: meal },
-            });
-          }}
-        />
-        <MainButton
-          buttonText="Main"
-          buttonSize="lg"
-          colorMode="dark"
-          buttonWidth="80%"
-          onClick={() => {
-            const meal = "main";
-            router.push({
-              pathname: "/search-select",
-              query: { meal: meal },
-            });
-          }}
-        />
-        <MainButton
-          buttonText="Dessert"
-          buttonSize="lg"
-          colorMode="dark"
-          buttonWidth="80%"
-          onClick={() => {
-            const meal = "dessert";
-            router.push({
-              pathname: "/search-select",
-              query: { meal: meal },
-            });
-          }}
-        />
+        <section className="w-screen max-w-lg space-y-4 text-center">
+          <MainButton
+            buttonText="Breakfast"
+            buttonSize="lg"
+            colorMode="dark"
+            buttonWidth="80%"
+            onClick={() => {
+              const meal = "breakfast";
+              router.push({
+                pathname: "/search-select",
+                query: { meal: meal },
+              });
+            }}
+          />
+          <MainButton
+            buttonText="Main"
+            buttonSize="lg"
+            colorMode="dark"
+            buttonWidth="80%"
+            onClick={() => {
+              const meal = "main";
+              router.push({
+                pathname: "/search-select",
+                query: { meal: meal },
+              });
+            }}
+          />
+          <MainButton
+            buttonText="Dessert"
+            buttonSize="lg"
+            colorMode="dark"
+            buttonWidth="80%"
+            onClick={() => {
+              const meal = "dessert";
+              router.push({
+                pathname: "/search-select",
+                query: { meal: meal },
+              });
+            }}
+          />
+        </section>
       </VStack>
     </main>
   );
