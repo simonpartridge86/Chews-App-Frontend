1ffa5de5b01957aa969cf4062fa8edf581ef2f70
simonpartridge86 <simonpartridge86@gmail.com>
2022-08-03 14:08:34 +0100

    fixing 404 logo import

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,19 @@
 // pages/404.js
-import Link from 'next/link';
+import Link from "next/link";
+import ChewsLogo from "../components/chews-logo";
 
 export default function Custom404() {
   return (
     <>
-        <h1>404 - Page Not Found</h1>
-        <h2>Uh-Oh.. looks like your Chews-ing ain't correct.</h2>
-        <ChewsLogo theme={"brand.primary"} size={"3xl"} />
-        <p>Home <Link href="/" ><a>Home</a></Link> </p>
+      <h1>404 - Page Not Found</h1>
+      <h2>Uh-Oh.. looks like your Chews-ing ain't correct.</h2>
+      <ChewsLogo theme={"brand.primary"} size={"3xl"} />
+      <p>
+        Home{" "}
+        <Link href="/">
+          <a>Home</a>
+        </Link>{" "}
+      </p>
     </>
-)}
+  );
+}
