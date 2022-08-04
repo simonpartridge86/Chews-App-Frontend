dae803f0c354c6b020c642d719f95f046aa5696c
simonpartridge86 <simonpartridge86@gmail.com>
2022-08-04 19:30:42 +0100

    adding functionality to radio buttons

--- components/FilterModal.js
+++ components/FilterModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import MainButton from "./MainButton";
 import {
   Modal,
@@ -11,27 +11,106 @@ import {
   useDisclosure,
   Button,
   Text,
-  FormControl,
-  FormLabel,
   Switch,
-  SimpleGrid,
   Divider,
 } from "@chakra-ui/react";
 
+const dietArray = [
+  {
+    filter: "Vegan",
+    isChecked: false,
+  },
+  {
+    filter: "Gluten-free",
+    isChecked: false,
+  },
+  {
+    filter: "Vegetarian",
+    isChecked: false,
+  },
+  {
+    filter: "Dairy-free",
+    isChecked: false,
+  },
+];
+const cuisineArray = [
+  {
+    filter: "Italian",
+    isChecked: false,
+  },
+  {
+    filter: "Chinese",
+    isChecked: false,
+  },
+  {
+    filter: "Indian",
+    isChecked: false,
+  },
+  {
+    filter: "British",
+    isChecked: false,
+  },
+  {
+    filter: "American",
+    isChecked: false,
+  },
+  {
+    filter: "Moroccan",
+    isChecked: false,
+  },
+  {
+    filter: "Spanish",
+    isChecked: false,
+  },
+  {
+    filter: "Mexican",
+    isChecked: false,
+  },
+  {
+    filter: "Thai",
+    isChecked: false,
+  },
+];
+
+const filterArray = () => {
+  console.log(
+    cuisineArray.map((item) => {
+      return { filter: item, isChecked: false };
+    })
+  );
+};
+
 export default function FilterModal() {
-  const dietArray = ["Vegan", "Vegetarian", "Gluten-Free", "Dairy-Free"];
+  const [dietFilters, setDietFilters] = useState(filterArray());
   const { isOpen, onOpen, onClose } = useDisclosure();
+  function onChange(e, item) {
+    const index = filters.findIndex((object) => {
+      return object.filter === item;
+    });
+    const newArray = [...filters];
+    newArray[index].isChecked = e.target.checked;
+    setFilters(newArray);
+    console.log(filters);
+  }
   return (
     <>
       <Button onClick={onOpen}>Open Modal</Button>
 
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        motionPreset="scale"
+        scrollBehavior="inside"
+      >
         <ModalOverlay />
         <ModalContent
-          width="80vw"
+          width="90vw"
           flex="flex"
           flexDir="column"
           alignItems="center"
+          maxWidth={"lg"}
+          maxHeight={"70vh"}
         >
           <ModalHeader fontFamily={"brand.main"} fontSize={"2xl"}>
             <span className="font-permanent-marker text-center text-2xl text-primary-color font-normal">
@@ -40,7 +119,7 @@ export default function FilterModal() {
             Filters
           </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
+          <ModalBody className="w-[80%] max-h-[50%]">
             <Text
               fontFamily={"brand.main"}
               fontWeight={600}
@@ -51,29 +130,78 @@ export default function FilterModal() {
               Dietary Preferences:
             </Text>
             <Divider />
-            <FormControl as={SimpleGrid} columns={2}>
+            <section className="w-[100%] flex flex-col mt-[1vh]">
               {dietArray.map((item) => {
                 return (
-                  <>
-                    <FormLabel
-                      fontFamily={"brand.main"}
-                      my={"1vh"}
-                      htmlFor={item}
-                    >
+                  <div
+                    className="flex flex-row items-center justify-between"
+                    key={item}
+                  >
+                    <label fontFamily={"brand.main"} htmlFor={item}>
                       {item}
-                    </FormLabel>
-                    <Switch colorScheme="teal" my={"1.5vh"} id={item} />
-                  </>
+                    </label>
+
+                    <Switch
+                      colorScheme="red"
+                      id={item}
+                      onChange={(e) => {
+                        onChange(e, item);
+                      }}
+                    />
+                  </div>
                 );
               })}
-            </FormControl>
+            </section>
+            <Text
+              fontFamily={"brand.main"}
+              fontWeight={600}
+              fontSize={"lg"}
+              textColor={"brand.primary"}
+              textAlign={"left"}
+              marginTop={"2vh"}
+            >
+              Dietary Preferences:
+            </Text>
+            <Divider />
+            <section className="w-[100%] flex flex-col mt-[1vh]">
+              {cuisineArray.map((item) => {
+                return (
+                  <div
+                    className="flex flex-row items-center justify-between"
+                    key={item}
+                  >
+                    <label fontFamily={"brand.main"} htmlFor={item}>
+                      {item}
+                    </label>
+
+                    <Switch
+                      colorScheme="red"
+                      id={item}
+                      onChange={(e) => {
+                        onChange(e, item);
+                      }}
+                      isChecked={true}
+                    />
+                  </div>
+                );
+              })}
+            </section>
           </ModalBody>
 
-          <ModalFooter>
+          <ModalFooter gap={"10px"}>
             <MainButton
               colorMode={"dark"}
               buttonText="Save"
               onClick={onClose}
+              buttonSize="md"
+              buttonWidth={"100px"}
+            />
+            <MainButton
+              colorMode={"light"}
+              buttonText="Cancel"
+              onClick={onClose}
+              buttonSize="md"
+              buttonWidth={"100px"}
             />
           </ModalFooter>
         </ModalContent>
